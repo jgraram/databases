743987afe2fb4c9909d08643281305bb2c1e9371
juangraram <j.graram@outlook.com>
2023-04-27 20:23:14 -0400

    Insert information in a table

---
+++ 00-sqlBasics/05-Insert_Information_table.sql
@@ -0,0 +1,4 @@
+--  insert a new record of data into three columns of a table called "Games" with the following values: 
+-- GameID = 1, gameDate = 2022-10-10 and score = 3
+
+INSERT INTO games (GameID, gameDate, score) VALUES (1, “2022-10-10”, 3);
